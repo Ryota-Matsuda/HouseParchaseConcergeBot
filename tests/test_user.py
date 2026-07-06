ceed9162eafd417414e7477706d2104f29c83203
良太 松田 <m.ryota.0917@outlook.jp>
2026-07-07 07:04:29 +0900

    整型

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,4 +1,3 @@
-
 from app.infra.db.models import User
 from app.infra.db.repository.user import UserRepository
 from app.schemas.dto import UserRecord
@@ -7,11 +6,7 @@ from app.schemas.dto import UserRecord
 def test_save(session):
     """新規のユーザーが 保存 されることを確認する"""
     user_repo = UserRepository(session)
-    user_record = UserRecord(
-        household_id=1,
-        name="Test User",
-        is_active=True
-    )
+    user_record = UserRecord(household_id=1, name="Test User", is_active=True)
 
     user_repo.save(user_record)
     session.commit()
